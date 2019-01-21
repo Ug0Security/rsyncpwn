f792b4b7e4dab2852704159c2bf98c2d60545b6d
Ug0Security <c.vallicari@gmail.com>
2019-01-21 17:39:02 +0100

    Create rsyncpwn.sh

---
+++ rsyncpwn.sh
@@ -0,0 +1,18 @@
+cat "iplist" | while read line
+do
+   echo "Test $line En Cours .."
+   torify rsync --list-only  $line:: > testtmp.txt
+   grep -Eo '^[^ ]+' testtmp.txt > testtmp2.txt
+   sed 's/^/'$line',/' testtmp2.txt >> share.txt
+   rm testtmp*
+done
+
+
+cat "share.txt" | while IFS=, read col1 col2
+do
+echo $col1/$col2 >> resultats.txt
+torify sshpass rsync --list-only  $col1::$col2/ >> resultats.txt
+
+done
+rm share.txt
+cat resultats.txt
